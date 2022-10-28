d891463e8ca7e7214dd1daaeb89aba39462199f1
Markit125 <tamarkus0@gmail.com>
2022-10-28 17:16:24 +0300

    10th done

--- Lab_2/10/10.c
+++ Lab_2/10/10.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define DEFAULT_COUNT_REMAINDERS 10
+#define FIND_TILL 20
 
 
 int is_endless(double n, int base)
@@ -13,37 +14,22 @@ int is_endless(double n, int base)
     }
 
     int rems_count = DEFAULT_COUNT_REMAINDERS;
-    int *rems = (int *) malloc(sizeof(int) * rems_count);
+    double *rems = (double *) malloc(sizeof(double) * rems_count);
+    
     if (rems == NULL)
     {
         return -1;
     }
 
-    double checker = n;
-    int divisor = 1;
+    rems[0] = n;
 
-    while (checker != (double) ((int) checker))
-    {
-        checker *= 10;
-        divisor *= 10;
-        if (checker > 300)
-        {
-            checker -= (((int) checker) / 1000) * 1000;
-        }
-    }
-
-    rems[0] = (int) checker;
-    int set[base - 1];
+    double rem;
     int i;
-    for (i = 0; i < base - 1; ++i)
+    int j;
+    for (i = 0; i < FIND_TILL; ++i)
     {
-        set[i] = 0;
-    }
-
-    int rem;
-    for (i = 0; i < 20; ++i)
-    {
-        rem = rems[i] % base;
+        rem = rems[i] * base;
+        rem -= (int) rem;
 
         if (rem == 0)
         {
@@ -53,27 +39,27 @@ int is_endless(double n, int base)
 
         rems[i + 1] = rem;
 
-        if (set[rem - 1] == 0)
+        for (j = 0; j < i; ++j)
         {
-            set[rem - 1] = 1;
-        }
-        else
-        {
-            free(rems);
-            return 1;
+            if (rems[j] == rem)
+            {
+                free(rems);
+                return 1;
+            }
         }
 
         if (i == rems_count - 1)
         {
             rems_count *= 2;
-            int *ptr = (int *) realloc(rems, sizeof(int) * rems_count);
+            double *ptr = (double *) realloc(rems, sizeof(double) * rems_count);
+
             if (ptr == NULL)
             {
                 free(rems);
                 return -1;
             }
+
             rems = ptr;
-            free(ptr);
         }
     }
 
@@ -96,11 +82,12 @@ int check_endless(double a[], int base, int count, ...)
     int i;
     double *ptr = a;
     double num;
+
     for (i = 0; i < count; ++i)
     {
         num = va_arg(ap, double);
         int endless = is_endless(num, base);
-        printf("%d\n", endless);
+        
         if (!endless)
         {
             *(ptr++) = num;
@@ -111,6 +98,7 @@ int check_endless(double a[], int base, int count, ...)
             return -1;
         }
     }
+
     va_end(ap);
     *(ptr) = -1;
 
@@ -123,7 +111,7 @@ int main()
     double arr[11];
     arr[10] = -1;
 
-    int err = check_endless(arr, 2, 3, (double) 0.8, (double) 0.5, (double) 0.23);
+    int err = check_endless(arr, 8, 3, (double) 0.74, (double) 0.5, (double) 0.5625);
 
     if (err == 1)
     {
@@ -138,6 +126,7 @@ int main()
 
     int i = 0;
     printf("These decimal numbers aren't endless in binary system:\n");
+
     while (arr[i] != -1)
     {
         printf("%f\n", arr[i++]);
