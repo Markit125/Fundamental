d076f25a486ef2049a9c1837bab79175b5bb825d
Markit125 <tamarkus0@gmail.com>
2023-03-10 07:11:05 +0300

    lab5-1

---
+++ Lab_5/build/1/CMakeFiles/lab5-1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/logger/concrete/logger_builder_concrete.cpp" "1/CMakeFiles/lab5-1.dir/logger/concrete/logger_builder_concrete.cpp.o" "gcc" "1/CMakeFiles/lab5-1.dir/logger/concrete/logger_builder_concrete.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/logger/concrete/logger_concrete.cpp" "1/CMakeFiles/lab5-1.dir/logger/concrete/logger_concrete.cpp.o" "gcc" "1/CMakeFiles/lab5-1.dir/logger/concrete/logger_concrete.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/logger/prototypes/logger.cpp" "1/CMakeFiles/lab5-1.dir/logger/prototypes/logger.cpp.o" "gcc" "1/CMakeFiles/lab5-1.dir/logger/prototypes/logger.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/logger/prototypes/logger_builder.cpp" "1/CMakeFiles/lab5-1.dir/logger/prototypes/logger_builder.cpp.o" "gcc" "1/CMakeFiles/lab5-1.dir/logger/prototypes/logger_builder.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/main.cpp" "1/CMakeFiles/lab5-1.dir/main.cpp.o" "gcc" "1/CMakeFiles/lab5-1.dir/main.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/time/my_time.cpp" "1/CMakeFiles/lab5-1.dir/time/my_time.cpp.o" "gcc" "1/CMakeFiles/lab5-1.dir/time/my_time.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
