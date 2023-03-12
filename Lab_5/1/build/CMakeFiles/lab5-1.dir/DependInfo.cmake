f2afbd2df848eb4440b170b7f9e6975b3ea72bfb
Markit125 <tamarkus0@gmail.com>
2023-03-12 18:23:35 +0300

    cmake

---
+++ Lab_5/1/build/CMakeFiles/lab5-1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/concrete/logger_builder_concrete.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/concrete/logger_builder_concrete.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/concrete/logger_builder_concrete.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/concrete/logger_concrete.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/concrete/logger_concrete.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/concrete/logger_concrete.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/configure/configure_file.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/configure/configure_file.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/configure/configure_file.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/prototypes/logger.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/prototypes/logger.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/prototypes/logger.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/prototypes/logger_builder.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/prototypes/logger_builder.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/logger/prototypes/logger_builder.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/main.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/main.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/main.cpp.o.d"
+  "/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/time/my_time.cpp" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/time/my_time.cpp.o" "gcc" "CMakeFiles/lab5-1.dir/home/markit125/AllData/Programming/CPlusPlus/Fundamental/Lab_5/1/source/time/my_time.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
