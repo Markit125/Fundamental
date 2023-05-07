16b201629cb6b695a09018daa08c796b32d08e3d
Markit125 <tamarkus0@gmail.com>
2023-05-07 21:41:51 +0300

    safe logger

---
+++ Lab_5/1/source/logger/complete/complete_logger.cpp
@@ -0,0 +1,13 @@
+#include "complete_logger.h"
+#include "logger.h"
+
+logging::logger const *complete_logger::safe_log(const std::string &message, logging::logger::severity severity) const {
+    
+    logging::logger *logger = get_logger();
+    
+    if (nullptr != logger) {
+        return logger->log(message, severity);
+    }
+
+    return nullptr;
+}
