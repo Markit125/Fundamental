abe3c5ef051582b9f8c69a354144f76a1cfcce83
Markit125 <tamarkus0@gmail.com>
2023-05-19 23:27:32 +0300

    CMakeLists

--- Lab_5/12/source/avl/avl.h
+++ Lab_5/12/source/avl/avl.h
@@ -2,6 +2,7 @@
 #define AVL_TREE_H
 
 #include "binary_search_tree.h"
+#include <cstddef>
 // #include "safe_allocator.h"
 // #include "complete_logger.h"
 
@@ -10,8 +11,7 @@ template<
     typename tvalue,
     typename tkey_comparer>
 class avl_tree:
-    public binary_search_tree<tkey, tvalue, tkey_comparer>
-{
+    public binary_search_tree<tkey, tvalue, tkey_comparer> {
     
 public:
 
@@ -33,8 +33,40 @@ public:
 
     ~avl_tree() override = default;
 
-    void get_something();
-    
+
+protected:
+
+    struct avl_tree_node : binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node {
+
+        size_t height;
+    };
+
+
+private:
+
+    size_t get_height(typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *node) const;
+
+    int balance_factor(typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *node) const;
+
+    void fix_height(typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **node);
+
+protected:
+
+    void after_insert_inner(
+            tkey const &key,
+            tvalue &&value,
+            typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *&subtree_root_address,
+            std::stack<typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **> &path_to_subtree_root_exclusive) override;
+
+    void after_remove_inner(
+            tkey const &key,
+            typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *&subtree_root_address,
+            std::stack<typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **> &path_to_subtree_root_exclusive) override;
+
+    void after_read_inner(
+            tkey const &key,
+            typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *&subtree_root_address,
+            std::stack<typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **> &path_to_subtree_root_exclusive) override;
 
 };
 
@@ -57,12 +89,67 @@ template<
     typename tkey,
     typename tvalue,
     typename tkey_comparer>
-void avl_tree<tkey, tvalue, tkey_comparer>::get_something() {
-    this->begin_prefix();
+size_t avl_tree<tkey, tvalue, tkey_comparer>::get_height(
+    typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *node) const {
+    
+    return node ? (node->height) : 0;
 }
 
+template<
+    typename tkey,
+    typename tvalue,
+    typename tkey_comparer>
+int avl_tree<tkey, tvalue, tkey_comparer>::balance_factor(
+    typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *node) const {
+    
+    return get_height(node->left_subtree_address) - get_height(node->right_subtree_address);
+}
 
+template<
+    typename tkey,
+    typename tvalue,
+    typename tkey_comparer>
+void avl_tree<tkey, tvalue, tkey_comparer>::fix_height(
+    typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **node) {
+    
+    (*node)->height = max(get_height((*node)->left_subtree_address), get_height((*node)->right_subtree_address)) + 1;
+}
 
+template<
+    typename tkey,
+    typename tvalue,
+    typename tkey_comparer>
+void avl_tree<tkey, tvalue, tkey_comparer>::after_insert_inner(
+    tkey const &key,
+    tvalue &&value,
+    typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *&subtree_root_address,
+    std::stack<typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **> &path_to_subtree_root_exclusive) {
 
+    
+}
+
+template<
+    typename tkey,
+    typename tvalue,
+    typename tkey_comparer>
+void avl_tree<tkey, tvalue, tkey_comparer>::after_remove_inner(
+    tkey const &key,
+    typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *&subtree_root_address,
+    std::stack<typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **> &path_to_subtree_root_exclusive) {
+
+    
+}
+
+template<
+    typename tkey,
+    typename tvalue,
+    typename tkey_comparer>
+void avl_tree<tkey, tvalue, tkey_comparer>::after_read_inner(
+    tkey const &key,
+    typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node *&subtree_root_address,
+    std::stack<typename binary_search_tree<tkey, tvalue, tkey_comparer>::tree_node **> &path_to_subtree_root_exclusive) {
+
+    
+}
 
 #endif
